788fd258829d8c1ea40f6ced735de550522bb956
a4ther <leealfsoto@gmail.com>
2015-11-23 11:39:14 -0600

    Prueba email Parche #1

--- enviarCorreo.php
+++ enviarCorreo.php
@@ -1,29 +1,31 @@
-// Pear Mail Library
-require_once "Mail.php";
+<?php
+	// Pear Mail Library
+	require_once "Mail.php";
 
-$from = 'fernandezmartin.eduardo77@gmail.com';
-$to = 'lsoto@soin.co.cr';
-$subject = 'Hola!';
-$body = "Hola Lee!";
+	$from = 'fernandezmartin.eduardo77@gmail.com';
+	$to = 'lsoto@soin.co.cr';
+	$subject = 'Hola!';
+	$body = "Hola Lee!";
 
-$headers = array(
-    'From' => $from,
-    'To' => $to,
-    'Subject' => $subject
-);
+	$headers = array(
+		'From' => $from,
+		'To' => $to,
+		'Subject' => $subject
+	);
 
-$smtp = Mail::factory('smtp', array(
-        'host' => 'ssl://smtp.gmail.com',
-        'port' => '465',
-        'auth' => true,
-        'username' => 'fernandezmartin.eduardo77@gmail.com',
-        'password' => 'Hondainvicta23'
-    ));
+	$smtp = Mail::factory('smtp', array(
+			'host' => 'ssl://smtp.gmail.com',
+			'port' => '465',
+			'auth' => true,
+			'username' => 'fernandezmartin.eduardo77@gmail.com',
+			'password' => 'Hondainvicta23'
+		));
 
-$mail = $smtp->send($to, $headers, $body);
+	$mail = $smtp->send($to, $headers, $body);
 
-if (PEAR::isError($mail)) {
-    echo('<p>' . $mail->getMessage() . '</p>');
-} else {
-    echo('<p>Message successfully sent!</p>');
-}
+	if (PEAR::isError($mail)) {
+		echo('<p>' . $mail->getMessage() . '</p>');
+	} else {
+		echo('<p>Message successfully sent!</p>');
+	}
+?>

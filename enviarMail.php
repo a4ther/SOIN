bd48a4b01ee64d54110f7e336567d38aecf7e3a8
a4ther <leealfsoto@gmail.com>
2015-11-23 15:15:07 -0600

    Parche
    Parche

--- enviarMail.php
+++ enviarMail.php
@@ -1,31 +1,12 @@
 <?php
-	// Pear Mail Library
-	require_once "Mail.php";
-
-	$from = 'fernandezmartin.eduardo77@gmail.com';
-	$to = 'lsoto@soin.co.cr';
-	$subject = 'Hola!';
-	$body = "Hola Lee!";
-
-	$headers = array(
-		'From' => $from,
-		'To' => $to,
-		'Subject' => $subject
-	);
-
-	$smtp = Mail::factory('smtp', array(
-			'host' => 'ssl://smtp.gmail.com',
-			'port' => '465',
-			'auth' => true,
-			'username' => 'fernandezmartin.eduardo77@gmail.com',
-			'password' => 'Hondainvicta23'
-		));
-
-	$mail = $smtp->send($to, $headers, $body);
-
-	if (PEAR::isError($mail)) {
-		echo('<p>' . $mail->getMessage() . '</p>');
-	} else {
-		echo('<p>Message successfully sent!</p>');
-	}
+	require 'vendor/autoload.php';
+	$sendgrid = new SendGrid('app44193749@heroku.com', 'j1mvpwjh5536');
+	
+	$message = new SendGrid\Email();
+	$message->addTo('lsoto@soin.co.cr')->
+	          setFrom($_POST['txtEmail'])->
+	          setSubject($_POST['txtAsunto'])->
+	          setText($_POST['txtMensaje'])->
+	          setHtml('<strong>Hello World!</strong>');
+	$response = $sendgrid->send($message);
 ?>

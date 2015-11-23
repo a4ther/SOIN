f3dec61d21a5620582b125b8022017635575f4cf
a4ther <leealfsoto@gmail.com>
2015-11-23 16:24:56 -0600

    Parche
    Parche

--- enviarMail.php
+++ enviarMail.php
@@ -1,13 +1,17 @@
 <?php
-	require 'vendor/autoload.php';
-	$sendgrid = new SendGrid('app44193749@heroku.com', 'j1mvpwjh5536');
-	
-	$message = new SendGrid\Email();
-	$message->addTo('lsoto@soin.co.cr')->
-	          setFrom('mrodriguez@soin.co.cr')->
-	          setSubject('Asunto')->
-	          setText('Hablada')->
-	          setHtml('<strong>Hello World!</strong>');
-	$response = $sendgrid->send($message);
-	echo $response;
+	try {
+	    require 'vendor/autoload.php';
+		$sendgrid = new SendGrid('app44193749@heroku.com', 'j1mvpwjh5536');
+		
+		$message = new SendGrid\Email();
+		$message->addTo('lsoto@soin.co.cr')->
+		          setFrom('mrodriguez@soin.co.cr')->
+		          setSubject('Asunto')->
+		          setText('Hablada')->
+		          setHtml('<strong>Hello World!</strong>');
+		$response = $sendgrid->send($message);
+		echo $response;
+	} catch (Exception $e) {
+	    echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
 ?>

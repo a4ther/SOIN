d761da7e5aa3490da252137f9333348faefe9c49
a4ther <leealfsoto@gmail.com>
2015-11-23 17:36:16 -0600

    Parche
    Parche

--- enviarMail.php
+++ enviarMail.php
@@ -1,15 +1,16 @@
 <?php
-	require 'vendor/autoload.php';
-	$sendgrid = new SendGrid('app44193749@heroku.com', 'j1mvpwjh5536');
-	$message = new SendGrid\Email();
-	$message->addTo('lsoto@soin.co.cr')->
-	          setFrom($_POST['txtEmail'])->
-	          setSubject($_POST['txtAsunto'])->
-	          setText($_POST['txtMensaje'])->
-	          setHtml('<strong>'.$_POST['txtMensaje'].'</strong>');
-	$response = $sendgrid->send($message);
-	
-	$json = json_decode($response, true);
-	echo $json['response']['message'];
-	var_dump($json['response']['message']);
+	try {
+	    require 'vendor/autoload.php';
+		$sendgrid = new SendGrid('app44193749@heroku.com', 'j1mvpwjh5536');
+		$message = new SendGrid\Email();
+		$message->addTo('lsoto@soin.co.cr')->
+		          setFrom($_POST['txtEmail'])->
+		          setSubject($_POST['txtAsunto'])->
+		          setText($_POST['txtMensaje'])->
+		          setHtml('<strong>'.$_POST['txtMensaje'].'</strong>');
+		$response = $sendgrid->send($message);
+		echo 1;
+	} catch(\SendGrid\Exception $e) {
+	    echo 0;
+	}
 ?>
